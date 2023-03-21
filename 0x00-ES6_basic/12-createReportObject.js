1f565d87dfeb1187f7dc391533605631e0f41ad3
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-21 03:14:13 +0300

    func

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,14 @@
+export default function createReportObject(employeesList) {
+    const departments = {};
+    for (const [departmentName, employees] of Object.entries(employeesList)) {
+      departments[departmentName] = [...employees];
+    }
+  
+    return {
+      allEmployees: departments,
+      getNumberOfDepartments() {
+        return Object.keys(departments).length;
+      },
+    };
+  }
+  
