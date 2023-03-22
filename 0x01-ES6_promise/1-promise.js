c29b56fc860fbf7a3510ae638d5ece7be0f1b463
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-22 19:17:23 +0300

    linting

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,11 @@
-/*resolve or reject*/
+/* resolve or reject */
+
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve ({ status: 200, body: 'Success'});
-        } else {
-            reject(new Error ('The fake API is not working currently'));
-        }
-    });
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
