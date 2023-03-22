d44477b38d87fa518d5b2ad2cbe1f0258e75561f
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-22 15:30:15 +0300

    improved

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-    return new Promise ((resolve, reject) => {
-        promise.then((response) => {
-            console.log('Got a response from the API');
-            resolve ({status: 200, body: 'success'});
-        })
-        .catch(() => {
-            reject(new Error());
-        });    
-    });
+    return promise
+    .then(() => {
+        console.log('Got a response from the API');
+        return { status: 200, body: 'success' }
+    })
+    .catch(() => new Error());
 }   
