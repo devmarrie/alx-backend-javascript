42affdf6c0346d3e2ca93b32b44bc834be4cf558
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-29 23:26:26 +0300

    student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,45 @@ const teacher3: Teacher = {
   };
 
 console.log(teacher3);
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+//function interface
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher:printTeacherFunction = (firstname, lastname) => `${firstname.charAt(0)}.${lastname}`
+
+
+//class
+interface classInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+interface User {
+    (firstName: string, lastName: string): classInterface;
+}
+class StudentClass implements classInterface {
+    firstname: string;
+    lastname: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstname = firstName;
+        this.lastname = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstname;
+    }
+}
+
+const user = new StudentClass('Marrie', 'Akeyo');
+console.log(user.displayName())
+console.log(user.workOnHomework())
