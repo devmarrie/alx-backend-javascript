38e37fb807e1e049a904171545e0f82200321f5a
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-31 14:37:09 +0300

    function spesific to employee

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -2,37 +2,41 @@ interface DirectorInterface {
     workFromHome(): string;
     getCoffeeBreak(): string;
     workDirectorTasks(): string;
-  }
+}
   
-  interface TeacherInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workTeacherTasks(): string;
-  }
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
   
-  export class Director implements DirectorInterface {
-    workFromHome = () => 'Working from home';
-    getCoffeeBreak = () => 'Getting a coffee break';
-    workDirectorTasks = () => 'Getting to director tasks';
-  }
+export class Director implements DirectorInterface {
+  workFromHome = () => 'Working from home';
+  getCoffeeBreak = () => 'Getting a coffee break';
+  workDirectorTasks = () => 'Getting to director tasks';
+}
   
-  export class Teacher implements TeacherInterface {
-    workFromHome = () => 'Cannot work from home';
-    getCoffeeBreak = () => 'Cannot have a break';
-    workTeacherTasks = () => 'Getting to work';
-  }
+export class Teacher implements TeacherInterface {
+  workFromHome = () => 'Cannot work from home';
+  getCoffeeBreak = () => 'Cannot have a break';
+  workTeacherTasks = () => 'Getting to work';
+}
   
-  export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director()
+export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director()
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
 
-  export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+// A Director function
+export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
     return (employee as Director).workDirectorTasks !== undefined;
-  }
+}
   
-  export function executeWork(employee: DirectorInterface | TeacherInterface): string {
-    let res = undefined;
-    (isDirector(employee)) ? res = employee.workDirectorTasks() : res = employee.workTeacherTasks();
-    return res;
-  }
+// Excecute according to role
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  let res = undefined;
+  (isDirector(employee)) ? res = employee.workDirectorTasks() : res = employee.workTeacherTasks();
+  return res;
+}
   type Subjects = "Math" | "History";
   
   export function teachClass(todayClass: Subjects): string {
@@ -45,3 +49,4 @@ interface DirectorInterface {
   
   console.log(teachClass("Math"));
   console.log(teachClass("History"));
+
