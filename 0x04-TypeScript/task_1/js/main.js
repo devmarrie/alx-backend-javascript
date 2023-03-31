f900ab5a356551bc4c9240b5700dc0c9e75e7151
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-31 14:38:37 +0300

    i dont understand this

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -6,3 +6,20 @@ var teacher3 = {
     contract: false,
 };
 console.log(teacher3);
+var printTeacher = function (firstname, lastname) { return "".concat(firstname.charAt(0), ".").concat(lastname); };
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstname = firstName;
+        this.lastname = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return 'Currently working';
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstname;
+    };
+    return StudentClass;
+}());
+var user = new StudentClass('Marrie', 'Akeyo');
+console.log(user.displayName());
+console.log(user.workOnHomework());
