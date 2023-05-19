8bd7f6f99aa6ca7238246453b7a0dc68fb050638
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-19 22:32:27 +0300

    initialising the repo

--- 6unittest/0-calcul.js
+++
@@ -1,7 +0,0 @@
-function calculateNumber(a, b) {
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
-  return roundedA + roundedB;
-}
-
-module.exports = calculateNumber;
--- 6unittest/0-calcul.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
-
-describe('test suite calculating the number', () => {
-  it('given 1,3 will return 4', () => {
-     assert.equal(calculateNumber(1, 3), 4);
-    });
-});
