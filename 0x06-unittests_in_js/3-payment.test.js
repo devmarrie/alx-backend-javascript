67eda86929952988a0811eb4a43c4559765e7d77
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-20 00:43:38 +0300

    finalising the test

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,9 +1,12 @@
 const sinon = require('sinon');
-const utils = require('./utils')
+const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment')
 
 describe('sendPaymentRequestToApi', function() {
   it('should validate usage of util', function() {
-     const watch = sinon.spy(sendPaymentRequestToApi)
-  })
-})
+    const watch = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20)
+    sinon.assert.calledWith(watch, 'SUM', 100, 20);
+    watch.restore();
+  });
+});
