21280f927bc6cfa8bd8a3d68f6dff10ba6bbf5ff
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-20 01:14:50 +0300

    tub and spy

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment')
+
+describe('sendPaymentRequestToApi', function() {
+  it('should validate usage of stub', function() {
+    const stubSinon = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consolelogSpy = sinon.stub(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWith(stubSinon, 'SUM', 100, 20);
+    sinon.assert.calledWith(consolelogSpy, 'The total is:10');
+    stubSinon.restore();
+    consolelogSpy.restore()
+  });
+});
