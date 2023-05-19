aac4acec172830850ecbcfc5d982b35b879eac12
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-20 01:16:18 +0300

    spicy stub

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,7 +5,7 @@ const sendPaymentRequestToApi = require('./4-payment')
 describe('sendPaymentRequestToApi', function() {
   it('should validate usage of stub', function() {
     const stubSinon = sinon.stub(Utils, 'calculateNumber').returns(10);
-    const consolelogSpy = sinon.stub(console, 'log');
+    const consolelogSpy = sinon.spy(console, 'log');
     sendPaymentRequestToApi(100, 20);
     sinon.assert.calledWith(stubSinon, 'SUM', 100, 20);
     sinon.assert.calledWith(consolelogSpy, 'The total is:10');
