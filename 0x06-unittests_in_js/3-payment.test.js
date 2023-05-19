a9825071c840261a7230d780b741b50b0ac56643
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-19 23:53:06 +0300

    payment test

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,9 @@
+const sinon = require('sinon');
+const utils = require('./utils')
+const sendPaymentRequestToApi = require('./3-payment')
+
+describe('sendPaymentRequestToApi', function() {
+  it('should validate usage of util', function() {
+     const watch = sinon.spy(sendPaymentRequestToApi)
+  })
+})
