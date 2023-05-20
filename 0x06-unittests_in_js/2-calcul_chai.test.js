e6e17f608c09bd42ba1ebdef562f8758fc385cf6
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-20 18:00:12 +0300

    separating the tests

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,21 +1,28 @@
 const assert = require('assert');
 const { expect } = require('chai')
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
-describe('Using expect from chai', function() {
+describe('SUM', function() {
     it('should calculate the sum as required', function() {
         expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-    })
+    });
+});
 
+describe('SUBTRACT', function() {
     it('should calculate the subtract as required', function() {
         expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal (-4);
-    })
+    });
+});
 
-    it('should calculate the sum as required', function() {
+describe('DIVIDE', function() {
+    it('should calculate the divide as required', function() {
         expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal (0.2);
-    })
+    });
+});
 
+describe('DIVIDE BY 0', function() {
     it('should calculate the divide by zero', function() {
         expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal ('Error');
-    })
-})
+    });
+});
+
