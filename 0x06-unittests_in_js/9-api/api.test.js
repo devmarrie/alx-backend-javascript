c26193f1a2a078e8a0c8c6ae46315c87efa4e783
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-20 18:24:43 +0300

    done not defined

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -28,7 +28,7 @@ describe('Testing express home route', function() {
 });
 
 describe('Adding a new test suite for the cart page', function() {
-  it('should check the status code when id is a number', function() {
+  it('should check the status code when id is a number', function(done) {
     const url = 'http://localhost:7865/cart/123';
     request(url, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
@@ -36,7 +36,7 @@ describe('Adding a new test suite for the cart page', function() {
     });
   });
 
-  it('should check status code when not  number', function() {
+  it('should check status code when not number', function(done) {
     const url = 'http://localhost:7865/cart/qwerty';
     request(url, (err, res, body) => {
       expect(res.statusCode).to.equal(404);
