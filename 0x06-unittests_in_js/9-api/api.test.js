39ef0d6a2b56df629180ba7df12acf610e78d2ea
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-20 13:32:06 +0300

    testing the configured route

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,46 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Testing express home route', function() {
+    const url = 'http://localhost:7865/';
+    it('should have correct status code 200', function(done) {
+      request(url, (err, res, body) => {
+        if (err) throw err;
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('should have correct results', function(done) {
+      request(url, (err, res, body) => {
+        if (err) throw err;
+        expect(body).to.contain('Welcome to the payment system')
+        done();
+        });
+    });
+
+    it("check correct content length", function(done) {
+        request(url, function(err, res, body) {
+            expect(res.headers['content-length']).to.equal('29');
+            done();
+        });
+    });
+});
+
+describe('Adding a new test suite for the cart page', function() {
+  it('should check the status code when id is a number', function() {
+    const url = 'http://localhost:7865/cart/123';
+    request(url, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done()
+    });
+  });
+
+  it('should check status code when not  number', function() {
+    const url = 'http://localhost:7865/cart/qwerty';
+    request(url, (err, res, body) => {
+      expect(res.statusCode).to.equal(404);
+      done()
+    });
+  });
+});
